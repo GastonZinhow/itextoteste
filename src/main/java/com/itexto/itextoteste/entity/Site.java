8fce8f52c65879571341d001ceb02cbe168afcdf
Matheus Gaston <theteusviana@gmail.com>
2025-05-06 18:09:28 -0300

    fix: project done and working !

--- src/main/java/com/itexto/itextoteste/entity/Site.java
+++ src/main/java/com/itexto/itextoteste/entity/Site.java
@@ -5,7 +5,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Objects;
 
-@Document
+@Document(collection = "sites")
 public class Site {
     @Id
     private int id;
